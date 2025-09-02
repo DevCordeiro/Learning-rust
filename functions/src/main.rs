f79116d7214341b48a0f0b66b75710421ff7685a
Anderson Cordeiro <an.cordeiro28@gmail.com>
2025-09-02 20:06:47 -0300

    [feat]: Undestanding Statements and Expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
 }
 
 fn another_function(value: i32, unit_label: char) {
